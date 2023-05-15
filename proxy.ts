3bcdffafbd9a38e4bbe99618916d26cc97ad6bba
ykwen <ykwen@iflytek.com>
2023-05-15 14:24:36 +0800

    优化代码

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,18 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import { Server } from "https://deno.land/std/http/server.ts";
 
 const TARGET = "https://api.openai.com";
-
-serve(async (request: Request) => {
+const handler = async (request: Request) => {
   const url = new URL(request.url);
-  url.host = TARGET
+  const targetUrl = new URL(TARGET + url.pathname + url.search);
 
-  return await fetch(url, request)
-})
+  return await fetch(targetUrl.toString(), {
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
+  });
+};
+
+const server = new Server({ handler });
+console.log("server listening on http://localhost:80");
+
+await server.listenAndServe();
