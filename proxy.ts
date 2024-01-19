ee613548b444e2911b489d55fc4b6968da28b20a
wenyikun <wenyikun86@gmail.com>
2024-01-20 03:15:17 +0800

    处理代理被识别

--- proxy.ts
+++ proxy.ts
@@ -1,18 +1,11 @@
-import { Server } from "https://deno.land/std/http/server.ts";
-
-const TARGET = "https://api.openai.com";
-const handler = async (request: Request) => {
-  const url = new URL(request.url);
-  const targetUrl = new URL(TARGET + url.pathname + url.search);
-
-  return await fetch(targetUrl.toString(), {
-    method: request.method,
-    headers: request.headers,
-    body: request.body,
-  });
-};
-
-const server = new Server({ handler });
-console.log("server listening on http://localhost:80");
-
-await server.listenAndServe();
+Deno.serve(async (req) => {
+  const url = new URL(req.url)
+  return fetch("https://api.openai.com" + url.pathname, {
+    method: req.method,
+    headers: {
+      Authorization: req.headers.get('authorization'),
+      "Content-Type": "application/json"
+    },
+    body: req.body
+  })
+});
